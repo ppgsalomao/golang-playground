e53affd170d1539ef84e9753bdc07a2b04ac732c
Pedro Salomão <psalomao@inoa.com.br>
2019-12-29 16:12:10 -0300

    Second and Third exercises of Ninja Level 1's Tood McLeod How to Code course

--- how-to-code/ninja-level-1.go
+++ how-to-code/ninja-level-1.go
@@ -3,8 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("------------------\nExercise 1\n------------------")
 	exercise1()
+	fmt.Println("------------------\nExercise 2\n------------------")
+	exercise2()
+	fmt.Println("------------------\nExercise 3\n------------------")
+	exercise3()
 }
+
+// ------------------------------------------
+// Exercise 1
+// ------------------------------------------
 
 func exercise1() {
 	// int
@@ -20,4 +29,31 @@ func exercise1() {
 	fmt.Println(z)
 
 	fmt.Println("Value for x: ", x, "\nValue for y: ", y, "\nValue for z: ", z)
+}
+
+// ------------------------------------------
+// Exercise 2
+// ------------------------------------------
+
+var a int
+var b string
+var c bool
+
+func exercise2() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
+
+// ------------------------------------------
+// Exercise 3
+// ------------------------------------------
+
+var d int = 42
+var e string = "James Bond"
+var f bool = true
+
+func exercise3() {
+	s := fmt.Sprintf("%v\t%v\t%v", d, e, f)
+	fmt.Println(s)
 }
