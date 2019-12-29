f7cd968c602bf423b00606df0c9cf499955820f4
Pedro Salomão <psalomao@inoa.com.br>
2019-12-29 16:02:53 -0300

    First exercise of Ninja Level 1's Tood McLeod How to Code course

---
+++ how-to-code/ninja-level-1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	exercise1()
+}
+
+func exercise1() {
+	// int
+	x := 42
+	fmt.Println(x)
+
+	// string
+	y := "James Bond"
+	fmt.Println(y)
+
+	// boolean
+	z := true
+	fmt.Println(z)
+
+	fmt.Println("Value for x: ", x, "\nValue for y: ", y, "\nValue for z: ", z)
+}
